096ad1712db37afa824bb677bff484bf59b9ce95
rsyu <rustrafa@gmail.com>
2025-03-11 16:43:13 +0300

    Idea files

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    application
+}
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.13.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
