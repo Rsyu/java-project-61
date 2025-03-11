8e5120a2663aedd41fb3ada0327a3996ceb4b4bf
rsyu <rustrafa@gmail.com>
2025-03-11 17:49:48 +0300

    updates

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class App {
-        public static void main(String[] args) {
-            Cli.greetUser();
-        }
+    public static void main(String[] args) {
+        Cli.greetUser();
+    }
 }
