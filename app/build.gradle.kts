99131c2830112696e7d5a957a06af4538a407bca
rsyu <rustrafa@gmail.com>
2025-03-11 17:59:19 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.41.0"
     application
+    java
+    checkstyle
 }
 application {
     mainClass.set("hexlet.code.App")
